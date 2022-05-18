c77aa65c5e3a93c080c11521bdeb09c085a9a687
Stoyan Stoyanov <chaneto800ss@gmail.com>
2022-05-18 09:40:41 +0300

    update

--- src/main/resources/db/changelog/v1/changes/insert-data.sql
+++ src/main/resources/db/changelog/v1/changes/insert-data.sql
@@ -1,5 +1,5 @@
 INSERT INTO orders (product_id, product_name, quantity, created_date, last_modified_date)
-VALUES (1, 'Dell 5401', 12, '2020-05-20', '2020-05-20'),
+VALUES (1, 'Dell 5401', 12, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        (9, 'Samsung T22', 19, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        (7, 'Toshiba', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        (5, 'ASUS', 33, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
