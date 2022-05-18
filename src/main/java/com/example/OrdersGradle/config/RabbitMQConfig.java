0d412a624932239fe064eb89095424dd3bdde4a4
Stoyan Stoyanov <chaneto800ss@gmail.com>
2022-05-18 19:27:37 +0300

    update

--- src/main/java/com/example/OrdersGradle/config/RabbitMQConfig.java
+++ src/main/java/com/example/OrdersGradle/config/RabbitMQConfig.java
@@ -3,7 +3,6 @@ package com.example.OrdersGradle.config;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -37,6 +36,13 @@ public class RabbitMQConfig {
 
   @Bean
   public AmqpTemplate template(ConnectionFactory connectionFactory) {
+    RabbitTemplate template = new RabbitTemplate(connectionFactory);
+    template.setMessageConverter(converter());
+    return template;
+  }
+
+  @Bean
+  public AmqpTemplate template1(ConnectionFactory connectionFactory) {
     RabbitTemplate template = new RabbitTemplate(connectionFactory);
     template.setMessageConverter(converter());
     return template;
